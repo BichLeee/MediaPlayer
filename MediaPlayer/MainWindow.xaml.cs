1d560de6d595077eef0bd6bcd8cf732c5b136d9d
20120435 <bich1042002@gmail.com>
2022-12-23 10:42:19 +0700

    edit play button

--- MediaPlayer/MainWindow.xaml.cs
+++ MediaPlayer/MainWindow.xaml.cs
@@ -31,6 +31,8 @@ namespace MediaPlayer
             sidebar.SelectedIndex= 0;
         }
 
+        bool isPlay = false;
+
         private void sidebar_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var selected = sidebar.SelectedItem as NavButton;
@@ -41,6 +43,20 @@ namespace MediaPlayer
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isPlay == false)
+            {
+                PlayButton_Path.Data = Geometry.Parse("M6 5h4v14H6zm8 0h4v14h-4z");
+                isPlay = true;
+            }
+            else
+            {
+                PlayButton_Path.Data = Geometry.Parse("M4610 6399 l0 -2881 43 25 c195 114 4144 2392 4494 2593 339 194 448 262 440 270 -7 7 -743 434 -1637 949 -894 516 -2001 1155 -2460 1420 -459 265 -845 487 -857 494 l-23 12 0 -2882z");
+                isPlay = false;
+            }
         }
     }
 }
